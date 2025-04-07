5a2b4fe12f27b8ef9d0fc286566aacb6807c8492
localplayerr <ddlagd0@gmail.com>
2025-04-07 14:39:12 +0300

    Create Insanity.lua

---
+++ Insanity.lua
@@ -0,0 +1,8 @@
+local light = game.Lighting
+
+light.Sky.StarCount = 0
+
+light.ClockTime = 0
+light.OutdoorAmbient = Color3.fromRGB(0,0,0)
+light.Ambient = Color3.fromRGB(0,0,0)
+light.Brightness = 0
