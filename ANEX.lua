d11035bada56c336e82c89f613f9760a9bd29aea
localplayerr <ddlagd0@gmail.com>
2025-04-13 19:45:19 +0300

    Update ANEX.lua

--- ANEX.lua
+++ ANEX.lua
@@ -1,33 +1,86 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local Window = OrionLib:MakeWindow({Name = "ANEX Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "ANTIEXPLOITERHUB"})
+local Window = Rayfield:CreateWindow({
+   Name = "ANEX",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Please wait...",
+   LoadingSubtitle = "Made by LocalPlayer",
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
-function GetPlrs()
-  local playerss = {}
-  for _ , v in game.Players:GetPlayers() do
-    table.insert(playerss,v.Name)
-  end
-  return playerss
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "ANTEXPHUB"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+
+local playerss = {}
+for _ , v in game.Players:GetPlayers() do
+	table.insert(playerss,v.Name)
 end
 
-local Tab = Window:MakeTab({
-	Name = "Kill",
-	Icon = "rbxassetid://0",
-	PremiumOnly = false
+local Tab = Window:CreateTab("Kill", 0)
+
+local PlayerToKill = {}
+
+function Kill(player)	
+	local part = game.Players[player].Character.HumanoidRootPart
+	local fram = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+	
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame
+	task.wait(0.1)	
+	game.ReplicatedStorage.GrabEvents.SetNetworkOwner:FireServer(part, part.CFrame)
+	task.wait(0.2)
+	
+	part.Parent.Humanoid.Health = 0
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = fram
+end
+
+local chos = Tab:CreateDropdown({
+   Name = "Chose player",
+   Options = playerss,
+   CurrentOption = {game.Players.LocalPlayer.Name},
+   MultipleOptions = true,
+   Flag = "Dropdown1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Options)
+	PlayerToKill = Options
+   end,
 })
 
-local PlayerToKill = ""
-
-Tab:AddDropdown({
-	Name = "ChosePlayer",
-	Default = "1",
-	Options = {"1", "2"},
-	Callback = function(Value)
-		PlayerToKill = Value
-	end    
-})
-
-task.spawn(function()
-  ChosePlayer:Refresh(GetPlrs,true)
-  game.Players.Changed:connect(function() ChosePlayer:Refresh(GetPlrs,true)  end)
+game.Players.Changed:connect(function() 
+	local playerss = {}
+	for _ , v in game.Players:GetPlayers() do
+		table.insert(playerss,v.Name)
+	end
+	chos:Refresh(playerss)
 end)
+
+local KillPlayer = Tab:CreateButton({
+   Name = "Kill player",
+   Callback = function()
+   	for _ , plr in PlayerToKill() do
+		Kill(plr)
+	end
+   end,
+})
